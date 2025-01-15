67b19a2b3edf4842d694f57f2e2e18188d7c7ff2
UrePu <85550543+UrePu@users.noreply.github.com>
2025-01-15 21:24:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //---API---
 const api_token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTgxNzNjZTY4YTYyMWQ4OWI1ODQ5YjIwYzg5MzBmZiIsIm5iZiI6MTczNjMxNDY2Mi42ODQsInN1YiI6IjY3N2UwZjI2MjE4ZmQ1N2FjZjRlNTc1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UplQK5NQAiCLPzUCZqyZy0gueCx_R7Q6NPzHT1oIYtE";
+  
 
 const options = {
   method: "GET",
